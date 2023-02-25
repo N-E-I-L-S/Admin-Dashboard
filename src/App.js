6d3af8b776cbd8a35986fc7de2d24cbd4dcb01a3
Neil Shukla <neilpshukla221203@gmail.com>
2023-02-25 21:00:18 +0530

    topbar and sidebar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from './themes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import './App.css'
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className='app'>
+      <Sidebar isSidebar={isSidebar} />
+      <main className='content'>
+      <Topbar setIsSidebar={setIsSidebar} />
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
+      </main>
+      </div>
+    </ThemeProvider>
+   </ColorModeContext.Provider>
+   </>
   );
 }
 
